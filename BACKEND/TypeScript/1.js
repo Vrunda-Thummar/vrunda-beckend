6ed00787a38cb83e296dfeea80f3299adcfcdae7
Vrunda-Thummar <vrundathummar51@gmail.com>
2024-07-19 18:32:58 +0530

    19-07-24

---
+++ BACKEND/TypeScript/1.js
@@ -0,0 +1,6 @@
+// let a : number = 101; 
+var a = 101;
+// let a : number[] = 101; 
+// let a : Array<number> = [101]; 
+// let a : Object = [101]; 
+console.log(a);
---
+++ BACKEND/TypeScript/1.ts
@@ -0,0 +1,6 @@
+let a : number = 101; 
+// let a : any = 101; 
+// let a : number[] = 101; 
+// let a : Array<number> = [101]; 
+// let a : Object = [101]; 
+console.log(a);
